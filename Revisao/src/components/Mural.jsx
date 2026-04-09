53d1e57afcea678b24a2bd3be813719cc2bc4091
Gabyz777 <gabriellydiasmendes@gmail.com>
2026-04-09 08:32:18 -0300

    update2

--- Revisao/src/components/Mural.jsx
+++ Revisao/src/components/Mural.jsx
@@ -1,14 +1,22 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 export default function Mural() {
-const [tweets, setTweets] = useState(["Estudando muito!"]);
-const [novoTweet, setNovoTweet] = useState("React é show");
+  const [tweets, setTweets] = useState(["Estudando muito!"]);
+  const [novoTweet, setNovoTweet] = useState("React é show");
 
-const enviarTweet = () => {
-setTweets([...tweets, novoTweet]);
-setNovoTweet("");
+  const enviarTweet = () => {
+    setTweets([...tweets, novoTweet]);
+    setNovoTweet("Eae, blz?");
+  };
 
-};
-
-return <button onClick={enviarTweet}>Postar</button>;
+  return (
+    <div>
+      <ul>
+        {tweets.map((t, index) => (
+          <li key={index}>{t}</li>
+        ))}
+      </ul>
+      <button onClick={enviarTweet}>Postar</button>
+    </div>
+  );
 }
